340c383aa51e13638d39d3505ca270510dbf06ab
Lukas Riemer <lksriemer@gmail.com>
2020-01-14 14:06:22 +0100

    explained Macro export hack

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,15 @@
 
 extern crate lazy_static;
 
-// We need to pub use lazy_static, as global_(default_)counter! is expanded to a lazy_static! call. Absolute paths wont help here.
+// We need to pub use lazy_static, as global_(default_)counter! is expanded to a lazy_static! call. 
+// Absolute paths wont help here.
 // TODO: Think of a way to only pub reexport the lazy_static! macro.
 pub use lazy_static::*;
 
 // Hack for macro export.
+// In foreign crates, `global_counter::generic::Counter` will be the name of our counter, 
+// but in this crate (for testing), we need to artificially introduce this path.
+// TODO: Think of a better way to do this.
 #[doc(hidden)]
 pub mod global_counter{
     pub mod generic{
